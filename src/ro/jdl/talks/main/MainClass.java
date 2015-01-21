19bd7b38d7acfc60b2ff3d58c0006c7fff8e46de
Alexandru Onea <onea.alex@gmail.com>
2015-01-21 11:04:31 +0000

    main class

---
+++ src/ro/jdl/talks/main/MainClass.java
@@ -0,0 +1,12 @@
+package ro.jdl.talks.main;
+
+public class MainClass {
+
+	/*
+	 * This is the entry point of the application
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to pancake presentation tool");
+	}
+
+}
